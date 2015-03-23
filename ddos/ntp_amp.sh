08672d98f68a9a726a68aa5e72226e8d001d00e4
Valter Vasić <oikuda@users.noreply.github.com>
2015-03-23 14:49:55 +0100

    Update ntp_amp.sh

--- ddos/ntp_amp.sh
+++ ddos/ntp_amp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-# send a total of 1 000 000 * 10 packets using tcp replay
+# send a total of 1 000 000 * 16 packets using tcp replay
 sudo himage Attacker tcpreplay -i eth0 -l 1000000 ntp.pcap
