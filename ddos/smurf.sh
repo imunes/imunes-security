b39af605ba8a670658593e3d30c3fc13353c95cd
Miljenko Mikuc <miljenko.mikuc@fer.hr>
2018-09-06 15:02:19 +0200

    sysctl options for Linux

--- ddos/smurf.sh
+++ ddos/smurf.sh
@@ -9,5 +9,14 @@ exit()' > /tmp/smurf.scapy
 #copy the scapy script to the Attacker
 hcp /tmp/smurf.scapy Attacker:
 
+# enable icmp echo broadcasts (default: disabled)
+if test `uname -s` = "Linux"; then
+    for pc in n1 n2 n3 n4 n5 n6 n7 n8 n9 n10 n11 n12 n13 n14 n15 n16
+    do
+        echo -n $pc:
+        himage $pc sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=0
+    done
+fi
+
 #execute the script
 himage Attacker scapy -c smurf.scapy
